1f0ee5bc5625ad0d3125691697b34b91d49414ae
Kevin <kan@immersion.com>
2018-07-08 13:56:59 -0400

    Add @ToString(exclude recipe) to avoid stack overflow Add recipe contents to show.html

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,12 +2,14 @@ package guru.springframework.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @EqualsAndHashCode(exclude = {"recipe"})
+@ToString(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
