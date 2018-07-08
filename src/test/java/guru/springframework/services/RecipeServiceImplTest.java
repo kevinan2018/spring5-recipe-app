0a0d9dee82dd334aa6580ff54a56222193d91655
Kevin <kan@immersion.com>
2018-07-08 12:41:42 -0400

    Upgrade to bootstrap 4 css - index.html, show.html and pom.xml(webjars) Add findById methood to RecipeService Add RecipeController for show.html

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -9,12 +9,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -47,4 +47,20 @@ public class RecipeServiceImplTest {
         //RecipeRepository.findAll() called exactly 1 time!
         verify(recipeRepository, times(1)).findAll();
     }
+
+    public void getRecipeByIdTest() throws Exception {
+        /* Mock up the database for recipes */
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
 }
